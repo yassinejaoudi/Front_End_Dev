6a98d2997a03bf1e5a1ee5b491fc8cebfdacc685
yassinejaoudi <jaoudiyassine@gmail.com>
2018-04-13 01:13:35 -0400

    Add content of the website, feature of transition for the footer and also for the intro card

---
+++ Portfolio_Website/designs.js
@@ -0,0 +1,35 @@
+// const items = document.querySelectorAll('.albums');
+// const clickOverlay = document.querySelector('.clickOverlay');
+// const albumImage = clickOverlay.querySelector('.album__artwork');
+// // const albumImage = clickOverlay.querySelector('img');
+// const overlayClose = clickOverlay.querySelector('.close');
+
+// function handleClick(e) {
+//     const src = e.currentTarget.querySelector('img').src;
+//     albumImage.src = src;
+//     clickOverlay.classList.add('open');
+// }
+ 
+// function close() {
+//     album.classList.remove('open');
+// }
+
+// items.forEach(album => album.addEventListener('click', handleClick));
+
+// overlayClose.addEventListener('click', close);
+
+// jQuery(function($){
+    // $('.album__artwork').click(function(){
+        // var img = $(this).attr("src");
+        // var appear__image = "<div id = 'appear_image_div' onClick='closeImage()'></div>";
+        // appear__image = appear__image.concat("<img id = 'appear__image' src = '"+img+"' />");
+        // appear__image = appear__image.concat("<img id = 'close__image' onclick='closeImage()' src = 'close.png' />");
+        // $('body').append(appear__image);
+// 
+    // });
+// });
+// 
+// function closeImage(){
+    // $('#appear_image_div').remove();
+    // $('#appear_image').remove();
+// }
